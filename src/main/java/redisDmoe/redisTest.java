0fa1bcf36e1d5916e85c0e4a5872b56d908fdcab
liguangyao <2060421434@qq.com>
2022-07-27 17:13:03 +0800

    测试项目1

---
+++ src/main/java/redisDmoe/redisTest.java
@@ -0,0 +1,19 @@
+package redisDmoe;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.Set;
+
+public class redisTest {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("localhost");
+        jedis.auth("123456");
+        System.out.println("连接成功");
+        System.out.println("服务正在运行" + jedis.ping());
+        Set<String> set = jedis.keys("*");
+        for (String keys : set) {
+            System.out.println(keys);
+        }
+    }
+
+}
